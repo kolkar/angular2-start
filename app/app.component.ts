56f4579b8b7a356c1ea549a2beb58d7cf70c9736
Mykola Rabcheniuk <rabcheniukkolya@gmail.com>
2017-04-04 15:26:46 +0300

    Check, delete, ngif, ngfor, ngclass, form

---
+++ app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+
+class Todo {
+    constructor(public title: string,
+                public  completed: boolean=false) {}
+}
+
+
+const todos: Todo[] = [
+    {
+        title: 'Изучить JavaScript',
+        completed: true
+    },
+    {
+        title: 'Изучить Angular 2',
+        completed: false
+    },
+    {
+        title: 'Написать прилодение',
+        completed: false
+    }
+];
+
+@Component ({
+    moduleId: module.id,
+    selector: 'app',
+    templateUrl: 'app.components.html',
+    styleUrls: ['app.components.css']
+})
+export class AppComponent {
+    title: string = 'Angular 2Do';
+    todos: Todo[] = todos;
+    newTodoTitle: string = '';
+
+    create() {
+        let todo: Todo = new Todo(this.newTodoTitle);
+
+        this.todos.push(todo);
+        this.newTodoTitle = '';
+    }
+
+    toggle(todo: Todo ) {
+        todo.completed = !todo.completed;
+    }
+
+    delete(todo: Todo) {
+        let index = this.todos.indexOf(todo);
+
+        if (index > -1) {
+            this.todos.splice(index, 1);
+        }
+    }
+}
